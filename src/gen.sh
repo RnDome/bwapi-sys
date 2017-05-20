eca001acfe04ae60927dd65ae59d9ae288023e05
Dmitry Kashitsyn <korvin@deeptown.org>
2017-05-21 01:09:26 +0700

    Updates to latest BWAPIC, adds allow(non_snake_case)

--- src/gen.sh
+++ src/gen.sh
@@ -4,7 +4,7 @@ bindgen bridge.h -o bridge.rs \
   --blacklist-type "std.*|__.+|.+_$|Game_v(Send|Print|Draw).*|va_list|.+_t$" \
   --no-layout-tests \
   --no-derive-debug \
-  --raw-line "#![allow(improper_ctypes)]" \
+  --raw-line "#![allow(improper_ctypes, non_snake_case)]" \
   -- -I../../bwapi-c/include
 
 sed -i -r -- 's/.+\s+(.+)_;/pub struct \1;/' bridge.rs
