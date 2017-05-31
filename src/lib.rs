efd90dc2006aca36a0ca9ac4cf9534bbeb5d9a7c
Roman Proskuryakov <roman.proskuryakov@dev.zodiac.tv>
2017-05-31 04:24:21 +0300

    Fixes warnings in _Unwind* functions

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,15 @@ mod tests {
 }
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub extern fn _Unwind_Resume() {
-    println!("_Unwind_Resume from sys");
+    println!("_Unwind_Resume from bwapi-sys");
     process::abort();
 }
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub extern fn _Unwind_RaiseException() {
-    println!("_Unwind_Resume from sys");
+    println!("_Unwind_RaiseException from bwapi-sys");
     process::abort();
 }
